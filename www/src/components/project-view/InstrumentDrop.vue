683c602ff5de07b096c6a58c3f4fc7ad4b1246c3
Joe Davis <joecrush@msn.com>
2018-03-13 16:00:20 -0600

    initial instrument dropdown commit

--- www/src/components/project-view/InstrumentDrop.vue
+++ www/src/components/project-view/InstrumentDrop.vue
@@ -1,12 +1,25 @@
 <template>
     <div class="instrumentDrop">
+      <select v-model="inputSelect">
+        <option disabled value="">Instrument Input</option>
+        <!-- <option :value="" {{key.value}}</option> -->
+      </select> 
       
     </div>
   </template>
   
   <script>
+    import samples from "./../../assets/audio/samples.json"
     export default {
       name: 'InstrumentDrop',
+      data: {
+        inputSelect
+      },
+      computed: {
+        sampleSounds() {
+          return samples
+        }
+      }
     
     }
   </script>
