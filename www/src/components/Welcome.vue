cddf7ce7c027b823ef0d6cdfbc2e93d13fa21268
Joe Davis <joecrush@msn.com>
2018-03-14 15:12:50 -0600

    welcome updated with logo, tagline, modal for sign in

--- www/src/components/Welcome.vue
+++ www/src/components/Welcome.vue
@@ -1,22 +1,33 @@
 <template>
-  <div class="welcome text-dark">
-    <div class="pb-5">
-      <!-- <navBar></navBar> -->
+  <div class="welcome">
+    <div class="container-fluid">
+      <div class="row welcome-area align-items-center">
+        <div class="col-sm-12">
+          <div class="welcome-boxes">
+            <img src="../assets/images/beat-locker-logo-large-white.png" alt="beatlocker" class="logo-img">
+          </div>
+          <div class="welcome-boxes">
+            <img src="../assets/images/beat-locker-tagline-twoline-large-white.png" alt="beatlocker tagline" class="logo-img">
+          </div>
+          <div class="welcome-boxes">
+              <button class="btn btn-primary buttons-welcome px-4" @click="launchSignIn">Sign in</button>
+              <button class="btn btn-primary buttons-welcome px-4">Register</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <signIn v-on:showRegisterForm="showSignIn = false" v-on:closeSignInModal="showSignIn = false" v-if="showSignIn"></signIn>
 
-      <!-- <div class="welcome-message container">
-        <h1 class="text-center m-3">Welcome!</h1>
-        <p>Use this beat creating app to for music projects.</p>
-        <ul>
-          <li>Register/sign-in to access your private account</li>
-        </ul>
-      </div> -->
+    <!-- <div class="pb-5">
+
+
 
       <errorMsg v-if="authError.error">{{authError.message}}</errorMsg>
 
       <signIn v-on:showRegisterForm="showSignIn = false" v-if="showSignIn"></signIn>
 
       <register v-on:showSignInForm="showSignIn = true" v-if="!showSignIn"></register>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -35,7 +46,12 @@
     },
     data() {
       return {
-        showSignIn: true
+        showSignIn: false
+      }
+    },
+    methods: {
+      launchSignIn() {
+        this.showSignIn= true
       }
     },
     computed: {
@@ -48,17 +64,61 @@
 
 <style scoped>
   .welcome {
-    /* background-image: url('https://files.slack.com/files-pri/T1GGQN1RU-F9NS2NNLQ/image.png'); */
-
+    background-position: center;
+    background-image: url('../assets/images/beat-locker-splash-bg.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100vh;
+    width: 100%;
   }
+
   .welcome-message {
     max-width: 600px;
+  }
+
+  .welcome-area {
+    height: 90vh;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .logo-img {
+    width: 70%;
+  }
+
+  .tagline-img {
+    width: 60%;
+  }
+
+  .centerFlex {
+    align-items: center;
+    display: flex;
+    justify-content: center;
+  }
+
+  .welcome-boxes {
+    padding: 1rem;
+    margin: 1rem;
+    /* outline-color: blueviolet;
+    outline-style: dotted;
+    outline-width: 1px; */
+    align-items: center;
+    display: flex;
+    justify-content: center;
+
+  }
+
+  .buttons-welcome {
+    background-color: rgba(206, 33, 53, 1.0);
+    border-color: rgba(107, 32, 45, 1.0); 
+    color: rgba(251, 251, 251, 1.0);
+    margin:.5rem;
   }
 
   @media (min-width: 576px) {
     .welcome-message {
       width: 75%;
-      
+
     }
   }
 </style>
--- www/src/components/site-access/SignIn.vue
+++ www/src/components/site-access/SignIn.vue
@@ -1,26 +1,26 @@
 <template>
-  <div class="signIn container">
-    <div class="signInForm text-dark">
+  <div class="background">
+    <div class="signIn container">
+      <div class="signInForm text-dark">
 
-      <form class="border border-secondary rounded p-4" @submit.prevent="submit">
+        <form class="border border-secondary rounded p-4" @submit.prevent="submit">
 
-        <div class="form-group">
-          <label class="ml-4" for="email">Email:</label>
-          <input type="text" id="email" class="form-control" v-model="user.email" placeholder="my@address.com">
-        </div>
-        <div class="form-group">
-          <label class="ml-4" for="password">Password:</label>
-          <input type="password" id="password" class="form-control" v-model="user.password" placeholder="********">
-        </div>
+          <div class="form-group">
+            <label class="ml-4" for="email">Email:</label>
+            <input type="text" id="email" class="form-control" v-model="user.email" placeholder="my@address.com">
+          </div>
+          <div class="form-group">
+            <label class="ml-4" for="password">Password:</label>
+            <input type="password" id="password" class="form-control" v-model="user.password" placeholder="********">
+          </div>
 
-        <button class="btn btn-success px-4" type="submit">Sign in</button>
-        <div class="text-center pt-4">
-          <a href="#" class="text-muted" @click.prevent="showRegisterForm">Click here to register.</a>
-        </div>
-      </form>
-
-
-
+          <button class="btn btn-success px-4" type="submit">Sign in</button>
+          <button class="btn btn-success px-4" @click="closeModal">Close</button>
+          <div class="text-center pt-4">
+            <a href="#" class="text-muted" @click.prevent="showRegisterForm">Click here to register.</a>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
 </template>
@@ -42,6 +42,9 @@
       },
       showRegisterForm() {
         this.$emit('showRegisterForm')
+      },
+      closeModal() {
+        this.$emit('closeSignInModal')
       }
     }
   }
@@ -49,6 +52,18 @@
 </script>
 
 <style scoped>
+  .background {
+    background-color: rgba(0, 0, 0, .8);
+    position: fixed;
+    width: 100%;
+    height: 100vh;
+    top: 0;
+    left: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
   div.signIn {
     width: 95%;
   }
