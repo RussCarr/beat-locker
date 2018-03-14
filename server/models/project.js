3b611845c70ccf22d21968e124c9e2d7d2cc2893
Benjamen King <brk.3rdof3kings@gmail.com>
2018-03-14 10:31:01 -0600

    Update Project schema

--- server/models/project.js
+++ server/models/project.js
@@ -13,14 +13,47 @@ var schema = new Schema({
   },
   createdAt: {
     type: Number,
-    default: Date.now()
+    // default: Date.now() // <-- Do this on front end. Otherwise you don't actually get the current time.
   },
   userId: {
     type: Schema.Types.ObjectId,
     ref: 'User',
     required: true
   },
-  
+  trackIds: [String], // IDs of the Tracks that 'belong' to this Project
+  barCount: {
+    type: Number,
+    required: true,
+    default: 4
+  },
+  stepsPerBar: {
+    type: Number,
+    required: true,
+    default: 4
+  },
+  bpmSetting: {
+    type: Number,
+    required: true,
+    default: 120
+  },
+  privacySetting: {
+    type: String,
+    required: true,
+    default: 'private'
+  },
+  forkCount: Number,
+  playCount: Number,
+  shareCount: Number,
+  originalProject: {
+    projectId: {
+      type: Schema.Types.ObjectId,
+      ref: 'Project'
+    },
+    creatorId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  }
 })
 
 module.exports = mongoose.model(schemaName, schema)
