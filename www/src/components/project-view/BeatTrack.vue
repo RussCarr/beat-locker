7e906a7009309d9d45ecb1ac2fb000a76110769e
Joe Davis <joecrush@msn.com>
2018-03-15 19:55:55 -0600

    updated mute and solo styles

--- www/src/components/project-view/BeatTrack.vue
+++ www/src/components/project-view/BeatTrack.vue
@@ -9,7 +9,7 @@
       <div class="track-volume"> 
         <volumeSlider></volumeSlider>
       </div>
-      <div class="muteSolo">
+      <div>
         <div class="mute">mute</div>
         <div class="solo">solo</div>
       </div>
@@ -88,18 +88,34 @@
     max-width: 50px;
   }
 
-  .muteSolo {
+  .solo {
     color: rgba(251, 251, 251, 1.0);
     font-size: .7rem;
     outline-color: rgba(251, 251, 251, 1.0);
     outline-style: solid;
     outline-width: 1px;
+    padding-left:.2rem;
+    padding-right: .2rem;
+  }
+
+  .solo:hover,.solo:active{
+    background-color: rgba(206, 33, 53, 1.0);
+    cursor: pointer;
   }
 
   .mute {
+    color: rgba(251, 251, 251, 1.0);
+    font-size: .7rem;
     outline-color: rgba(251, 251, 251, 1.0);
     outline-style: solid;
     outline-width: 1px;
+    padding-left:.2rem;
+    padding-right: .2rem;
+  }
+
+  .mute:hover,.mute:active {
+    background-color: rgba(206, 33, 53, 1.0);
+    cursor: pointer;
   }
 
   .step {
@@ -109,7 +125,7 @@
   }
 
   .selected {
-    background-color: tomato;
+    background-color: rgba(229, 140, 148, 1.0);
   }
 
   .scrolling-wrapper {
