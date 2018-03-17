005be08960fdf317183563b08ff7df3da158cc3f
Joe Davis <joecrush@msn.com>
2018-03-16 20:08:57 -0600

    profile pics now working

--- www/src/components/profile/UserProfile.vue
+++ www/src/components/profile/UserProfile.vue
@@ -16,8 +16,9 @@
       <div class="row">
 
         <div class="col-4">
-          <img class="my-5 ml-5" :src="user.imgUrl">
-          
+          <div class="profile-img">
+            <img :src="user.imgUrl" class="img-fluid" alt="Responsive image">
+          </div>
         </div>
         <div class="col-7 mt-5">
           <ul>
@@ -55,9 +56,16 @@
   }
 </script>
 
-<style>
+<style scoped>
   .userProfile {
     color: white;
     display: flex;
+  }
+
+  .profile-img {
+    width: 250px;
+    height: 250px;
+    margin-left: 2rem;
+    margin-top: 2rem;
   }
 </style>
